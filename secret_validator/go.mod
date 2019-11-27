5ddc7c37309ea52ddbc74a3fd238ac180a2e738a
Rob Baruch <robertbaruch@mac.com>
2019-11-27 15:56:09 -0500

    checkpoint

--- secret_validator/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/rabarar/certificate-rotation/covalid
-
-go 1.13
-
-require github.com/dgryski/dgoogauth v0.0.0-20190221195224-5a805980a5f3
--- secrets_server/main.go
+++ secrets_server/main.go
@@ -5,7 +5,6 @@ import (
 	"crypto/x509"
 	"encoding/json"
 	"encoding/pem"
-	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -13,143 +12,28 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"sync"
 	"time"
 
-	otp "github.com/dgryski/dgoogauth"
+	cov "github.com/rabarar/covalid"
 	"github.com/rabarar/goca"
 )
-
-const (
-	DefaultHOTPCounter = 0
-	DefaultWindowSize  = 3
-)
-
-type OTPValidator struct {
-	validator otp.OTPConfig
-}
-
-type SecretHash struct {
-	hash map[string]*OTPValidator
-	sync.Mutex
-}
 
 var (
 	rootCAs     *x509.CertPool = nil
 	serialCount int64          = 0
 
-	secrets *SecretHash = nil
+	secrets *cov.SecretHash = nil
 )
 
-func NewSecretHash() *SecretHash {
-
-	sh := &SecretHash{}
-	sh.hash = make(map[string]*OTPValidator)
-
-	return sh
-}
-
-func (m *SecretHash) Add(key string, validator *OTPValidator) error {
-	m.Lock()
-	defer m.Unlock()
-
-	_, ok := m.hash[key]
-	if ok {
-		return errors.New("Key Exists, can't add")
-	}
-	m.hash[key] = validator
-	return nil
-}
-
-func (m *SecretHash) Delete(key string) {
-	m.Lock()
-	defer m.Unlock()
-
-	delete(m.hash, key)
-}
-
-func (m *SecretHash) Verify(key, token string) (bool, error) {
-	m.Lock()
-	defer m.Unlock()
-
-	otpVal, ok := m.hash[key]
-
-	if !ok {
-		return false, errors.New("No Key Exists")
-	}
-
-	ok, err := otpVal.validator.Authenticate(token)
-
-	c := otp.ComputeCode(otpVal.validator.Secret, int64(time.Now().Unix()/30))
-	fmt.Printf("EXPECTING: %06d - validating with [%s]\n", c, token)
-
-	if err != nil {
-		return false, errors.New("OTP Auth Failure")
-	}
-
-	return ok, nil
-}
-
-func NewOTPValidator(secret string, hoptCounter, windowSize int) *OTPValidator {
-	o := &OTPValidator{}
-	o.validator = otp.OTPConfig{
-		Secret:      secret,
-		HotpCounter: hoptCounter,
-		WindowSize:  windowSize,
-	}
-	return o
-}
+const (
+	PATH_SECRET = "/secret"
+)
 
 func defaultURLValue(val []string, defvalue string) string {
 	if len(val) > 0 {
 		return val[0]
 	}
 	return defvalue
-}
-
-// CertificateInfo returns a string describing the certificate
-func CertificateInfo(cert *x509.Certificate) string {
-	if cert.Subject.CommonName == cert.Issuer.CommonName {
-		return fmt.Sprintf("    Self-signed certificate %v\n", cert.Issuer.CommonName)
-	}
-
-	s := fmt.Sprintf("    Subject %v\n", cert.DNSNames)
-	s += fmt.Sprintf("    Serial No  %s\n", cert.SerialNumber.String())
-	s += fmt.Sprintf("    Issued by %s\n", cert.Issuer.CommonName)
-	return s
-}
-
-func getClientValidator(helloInfo *tls.ClientHelloInfo) func([][]byte, [][]*x509.Certificate) error {
-	return func(rawCerts [][]byte, verifiedChains [][]*x509.Certificate) error {
-		log.Printf("calling validator\n")
-		//copied from the default options in src/crypto/tls/handshake_server.go, 680 (go 1.11)
-		//but added DNSName
-		opts := x509.VerifyOptions{
-			Roots:         rootCAs,
-			CurrentTime:   time.Now(),
-			Intermediates: x509.NewCertPool(),
-			KeyUsages:     []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
-			DNSName:       helloInfo.ServerName,
-			//DNSName:       strings.Split(helloInfo.Conn.RemoteAddr().String(), ":")[0],
-		}
-
-		var err error
-		if len(verifiedChains) > 0 {
-			_, err = verifiedChains[0][0].Verify(opts)
-			log.Printf("DNSNames [RemoteAddr]: %s\n", helloInfo.Conn.RemoteAddr().String())
-			log.Printf("DNSNames [ServerName]: %s\n", helloInfo.ServerName)
-			if err != nil {
-				log.Printf("Verified failed: %s\n", err)
-				return err
-			} else {
-				log.Printf("Verified\n")
-				return nil
-			}
-		} else {
-			log.Printf("No Verified Chains, can't verifiy...\n")
-			return errors.New("No Verified Chains")
-		}
-	}
 }
 
 func main() {
@@ -163,7 +47,7 @@ func main() {
 
 	flag.Parse()
 
-	secrets = NewSecretHash()
+	secrets = cov.NewSecretHash()
 
 	issuer, err := goca.GetIssuer(*caKey, *caCert)
 	if err != nil {
@@ -203,26 +87,8 @@ func main() {
 	certAndKey, err := tls.X509KeyPair(certPEM, keyPEM)
 	log.Print(CertificateInfo(cert509))
 
-	config := &tls.Config{
-		GetConfigForClient: func(hi *tls.ClientHelloInfo) (*tls.Config, error) {
-			log.Printf("calling GetConfigForClient:\n")
-			serverConf := &tls.Config{
-				Certificates:          []tls.Certificate{certAndKey},
-				MinVersion:            tls.VersionTLS12,
-				ClientAuth:            tls.RequireAndVerifyClientCert,
-				ClientCAs:             rootCAs,
-				VerifyPeerCertificate: getClientValidator(hi),
-				ServerName:            *hostname,
-			}
-			serverConf.BuildNameToCertificate()
-			return serverConf, nil
-		},
-		PreferServerCipherSuites: true,
-		Certificates:             []tls.Certificate{certAndKey},
-	}
-
 	mux := http.NewServeMux()
-	mux.HandleFunc("/secret", func(w http.ResponseWriter, req *http.Request) {
+	mux.HandleFunc(PATH_SECRET, func(w http.ResponseWriter, req *http.Request) {
 		w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
 		w.Header().Add("content-type", "application/json")
 
@@ -230,6 +96,7 @@ func main() {
 		for k, v := range req.URL.Query() {
 			fmt.Printf("%s: %s\n", k, v)
 		}
+
 		params := req.URL.Query()
 		key := defaultURLValue(params["key"], "")
 		token := defaultURLValue(params["token"], "")
@@ -266,7 +133,7 @@ func main() {
 			secret := objmap["secret"]
 			log.Printf("Adding Validator with secret:[%s]\n", secret)
 
-			validator := NewOTPValidator(secret, DefaultHOTPCounter, DefaultWindowSize)
+			validator := cov.NewOTPValidator(secret, cov.DefaultHOTPCounter, cov.DefaultWindowSize)
 			err = secrets.Add(key, validator)
 			if err != nil {
 				fmt.Fprintf(w, "{\"status\":\"fail\"}\n")
@@ -281,7 +148,7 @@ func main() {
 	})
 
 	srv := &http.Server{
-		TLSConfig:    config,
+		TLSConfig:    cov.GetServerValidatonConfig(*hostname, certAndKey, rootCAs),
 		Addr:         *hostname + ":" + strconv.Itoa(*port),
 		Handler:      mux, //myHandler,
 		ReadTimeout:  time.Minute,
